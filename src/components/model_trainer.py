0390d8bf66e3aab4bad65d6daa9539feff407810
hari prasad <hariprasad9693@gmail.com>
2023-07-28 12:32:44 +0530

    Hyper-parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -9,12 +9,13 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from dataclasses import dataclass
 import os
 import sys
 from src.logger import logging
 from src.exeption import CustomException
-from src.utils import save_object,evaluate_model
+from src.utils import save_object,evaluate_models
 
 
 @dataclass
@@ -44,8 +45,46 @@ class Modeltrainer:
                 "AdaBoost Regressor": AdaBoostRegressor() 
                 
                 }
+            
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
         
-            model_report=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            model_report=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
 
 
             ## To get best model score from dict
@@ -78,6 +117,3 @@ class Modeltrainer:
         except Exception as e:
             raise CustomException(e,sys)
             
-
-
-
