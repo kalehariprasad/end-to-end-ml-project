36f20c1f5868907deb6a7b88276800dd3d71a91e
hari prasad <hariprasad9693@gmail.com>
2023-08-01 09:58:37 +0530

    app.py

--- src/utils.py
+++ src/utils.py
@@ -57,9 +57,10 @@ def evaluate_models(x_train, y_train, x_test, y_test, models, param):
 
 def load_object(file_path):
     try:
-        with open(file_path ,"rb") as file_obj:
-            return dill.load(file_obj)
-    except Exception as e :
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        print(f"Error occurred while loading the object from {file_path}: {e}")
         raise CustomException(e, sys)
 
         
